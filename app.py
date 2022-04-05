42ab9f8658ae6f948ca73ab5613cc83d64932b1e
Md.Moshiur Rahman <35825505+mhridoy@users.noreply.github.com>
2022-04-05 15:43:56 +0600

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,43 @@ hide_streamlit_style = """
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
-if (selected3=="Booking"):
+
+if (selected3=="Sign Up"):
+    st.title("Registration")
+    my_form = st.form(key='form-1')
+    # creating input fields
+    fname = my_form.text_input('First Name:')
+    lname = my_form.text_input('Last Name:')
+    email = my_form.text_input('Email:')
+    # creating radio button
+    gender = my_form.radio('Gender', ('Male', 'Female'))
+    # creating slider
+    age = my_form.slider('Age:', 1, 120)
+    # creating date picker
+    bday = my_form.date_input('Enter Birthdate:')
+    # creating a text area
+    address = my_form.text_area('Enter Address:')
+    # creating a submit button
+    submit = my_form.form_submit_button('Submit')
+    # the following gets updated after clicking on submit, printing the details of the fields that are submitted in the form
+    if(submit == 'Submit'):
+        st.write('Name is '+fname+' '+lname)
+        st.write('Email is '+email)
+        st.write('Gender is '+gender)
+        st.write('Age is '+str(age))
+        st.write('Birthday is '+str(bday))
+        st.write('Address is '+address)
+elif (selected3=="Login"):
+  st.title("Login")
+  my_form = st.form(key='form-1')
+  username = my_form.text_input("UserName")
+  passwords = my_form.text_input("Password")
+  submit = my_form.form_submit_button('Submit')
+  if(submit == 'Submit'):
+       # flag = False
+        st.balloons()
+        st.write("Welcome")
+elif (selected3=="Booking"):
       df= pd.read_csv("AllProductList.csv")
       st.header("Request a New Appointment")
       with st.form("my_form"):
